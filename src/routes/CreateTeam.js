9011950f9fcec582f9cd5937bb14c29aa45af027
Sameer Khan <sameer_khan@student.uml.edu>
2018-04-22 00:54:50 -0400

    Create team done

---
+++ src/routes/CreateTeam.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Form, Message, Button, Input, Container, Header } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class CreateTeam extends Component {
+  constructor(props) {
+    super(props);
+    extendObservable(this, {
+      name: '',
+      errors: {},
+    });
+  }
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this[name] = value;
+  }
+
+  onSubmit = async () => {
+    this.errors = {};
+    const { name } = this;
+    const response = await this.props.mutate({
+      variables: { name },
+    });
+
+    console.log(response);
+
+    const { ok, errors } = response.data.createTeam;
+
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+
+      this.errors = err;
+    }
+  }
+
+  render() {
+    const { name, errors: { nameError } } = this;
+    const errorList = [];
+    if (nameError) {
+      errorList.push(nameError);
+    }
+
+    return (
+      <Container text>
+        <Header as="h2">Create a Team</Header>
+        <Form>
+          <Form.Field error={!!nameError}>
+            <Input
+              name="name"
+              onChange={this.onChange}
+              value={name}
+              type="text"
+              placeholder="Team Name"
+              fluid
+            />
+          </Form.Field>
+          <Button onClick={this.onSubmit}>Submit</Button>
+        </Form>
+        {errorList.length ? (
+          <Message error header="There was some error with your submission" list={errorList} />
+        ) : null}
+      </Container>
+    );
+  }
+}
+
+const CREATE_TEAM = gql`
+  mutation($name: String!) {
+    createTeam(name: $name) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(CREATE_TEAM)(observer(CreateTeam));
