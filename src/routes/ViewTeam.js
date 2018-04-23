109be475f0715cadc08d66a4984b39eafdb9b62a
Sameer Khan <sameer_khan@student.uml.edu>
2018-04-23 17:38:04 -0400

    Added styling, looks dope

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -3,23 +3,26 @@ import React from 'react';
 import AppLayout from './components/AppLayout';
 import Channels from './components/Channels';
 import Header from './components/Header';
-import Input from './components/Input';
 import Messages from './components/Messages';
+import SendMessage from './components/SendMessage';
 import Teams from './components/Teams';
 
 export default () => (
   <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
+    <Teams teams={[{ id: '1', letter: 'T' }, { id: 2, letter: 'Q' }]} />
+    <Channels
+      teamname="Team name"
+      username="Username"
+      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
+    />
+    <Header channelName="general" />
     <Messages>
       <ul className="message-list">
         <li />
         <li />
       </ul>
     </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid Layout Module" />
-    </Input>
+    <SendMessage channelName="general" />
   </AppLayout>
 );
--- src/routes/components/Channels.js
+++ src/routes/components/Channels.js
@@ -1,7 +1,69 @@
+import React from 'react';
 import styled from 'styled-components';
 
-export default styled.div`
+const ChannelWrapper = styled.div`
   grid-column: 2;
   grid-row: 1 / 4;
   background-color: #4e3a4c;
+  color: #958993;
 `;
+
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
+
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
+
+const SideBarListItem = styled.li`
+  padding: 1.5px;
+  padding-left: 10px;
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const SideBarListHeader = styled.li`padding-left: 10px`;
+
+const PushLeft = styled.div`padding-left: 10px`;
+
+const Green = styled.span`color: #38978d`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : '⚬');
+
+const channel = ({ id, name }) => (
+  <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>
+);
+
+const user = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
+);
+
+export default ({
+  teamname, username, channels, users,
+}) => (
+  <ChannelWrapper>
+    <PushLeft>
+      <TeamNameHeader>{teamname}</TeamNameHeader>
+      {username}
+    </PushLeft>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
+        {channels.map(channel)}
+      </SideBarList>
+    </div>
+    <div>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
+        {users.map(user)}
+      </SideBarList>
+    </div>
+  </ChannelWrapper>
+);
